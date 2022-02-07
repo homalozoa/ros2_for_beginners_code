41cdb795412668c1e0a4df631c0cb928bae5f5fd
Homalozoa <nx.tardis@gmail.com>
2022-02-07 23:21:45 +0800

    [New] complete cpp_chatter & py_chatter to ch3
    Signed-off-by: Homalozoa <nx.tardis@gmail.com>

--- chatter_test/src/py_chatter/setup.py
+++ chatter_test/src/py_chatter/setup.py
@@ -4,7 +4,7 @@ package_name = 'py_chatter'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
