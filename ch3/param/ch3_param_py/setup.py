4b01b34936739fc314246c285f5aeb5f27a406e6
Homalozoa <nx.tardis@gmail.com>
2022-02-15 00:06:15 +0800

    [New] add ch3
    Signed-off-by: Homalozoa <nx.tardis@gmail.com>

---
+++ ch3/param/ch3_param_py/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'ch3_param_py'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'param'), glob('param/*.yaml'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Homalozoa',
+    maintainer_email='nx.tardis@gmail.com',
+    description='Package for testing parameters in Node',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'soliloquist = ch3_param_py.soliloquist:main',
+        ],
+    },
+)
