5750b132816c848bd3a4d5d90c79194fc6550c94
Homalozoa <nx.tardis@gmail.com>
2022-03-14 00:17:52 +0800

    [New] Add tf2 for ch4
    Signed-off-by: Homalozoa <nx.tardis@gmail.com>

---
+++ ch4/tf2/ch4_tf2_cpp/src/dynamic_transform.cpp
@@ -0,0 +1,80 @@
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <turtlesim/msg/pose.hpp>
+
+#include <memory>
+#include <string>
+
+using std::placeholders::_1;
+
+class DynamicTransform : public rclcpp::Node
+{
+public:
+  DynamicTransform()
+  : Node("turtle_tf2_frame_publisher")
+  {
+    // Declare and acquire `turtlename` parameter
+    this->declare_parameter<std::string>("turtlename", "turtle");
+    this->get_parameter("turtlename", turtlename_);
+
+    // Initialize the transform broadcaster
+    tf_broadcaster_ =
+      std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+    // Subscribe to a turtle{1}{2}/pose topic and call handle_turtle_pose
+    // callback function on each message
+    std::ostringstream stream;
+    stream << "/" << turtlename_.c_str() << "/pose";
+    std::string topic_name = stream.str();
+
+    subscription_ = this->create_subscription<turtlesim::msg::Pose>(
+      topic_name, 10,
+      std::bind(&DynamicTransform::handle_turtle_pose, this, _1));
+  }
+
+private:
+  void handle_turtle_pose(const turtlesim::msg::Pose & msg)
+  {
+    rclcpp::Time now = this->get_clock()->now();
+    geometry_msgs::msg::TransformStamped t;
+
+    // Read message content and assign it to
+    // corresponding tf variables
+    t.header.stamp = now;
+    t.header.frame_id = "world";
+    t.child_frame_id = turtlename_.c_str();
+
+    // Turtle only exists in 2D, thus we get x and y translation
+    // coordinates from the message and set the z coordinate to 0
+    t.transform.translation.x = msg.x;
+    t.transform.translation.y = msg.y;
+    t.transform.translation.z = 0.0;
+
+    // For the same reason, turtle can only rotate around one axis
+    // and this why we set rotation in x and y to 0 and obtain
+    // rotation in z axis from the message
+    tf2::Quaternion q;
+    q.setRPY(0, 0, msg.theta);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t);
+  }
+  rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+  std::string turtlename_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DynamicTransform>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ ch4/tf2/ch4_tf2_cpp/src/static_transform.cpp
@@ -0,0 +1,73 @@
+#include <memory>
+
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/static_transform_broadcaster.h"
+
+using std::placeholders::_1;
+
+class StaticTransform : public rclcpp::Node
+{
+public:
+  explicit StaticTransform(char * transformation[])
+  : Node("static_transform_node")
+  {
+    tf_publisher_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+    this->make_transforms(transformation);
+  }
+
+private:
+  void make_transforms(char * transformation[])
+  {
+    rclcpp::Time now = this->get_clock()->now();
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = now;
+    t.header.frame_id = "world";
+    t.child_frame_id = transformation[1];
+
+    t.transform.translation.x = atof(transformation[2]);
+    t.transform.translation.y = atof(transformation[3]);
+    t.transform.translation.z = atof(transformation[4]);
+    tf2::Quaternion q;
+    q.setRPY(
+      atof(transformation[5]),
+      atof(transformation[6]),
+      atof(transformation[7]));
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    tf_publisher_->sendTransform(t);
+  }
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_publisher_;
+};
+
+int main(int argc, char * argv[])
+{
+  auto logger = rclcpp::get_logger("logger");
+
+  // Obtain parameters from command line arguments
+  if (argc != 8) {
+    RCLCPP_INFO(
+      logger, "Invalid number of parameters\nusage: "
+      "ros2 run learning_tf2_cpp static_turtle_tf2_broadcaster "
+      "child_frame_name x y z roll pitch yaw");
+    return 1;
+  }
+
+  // As the parent frame of the transform is `world`, it is
+  // necessary to check that the frame name passed is different
+  if (strcmp(argv[1], "world") == 0) {
+    RCLCPP_INFO(logger, "Your static turtle name cannot be 'world'");
+    return 1;
+  }
+
+  // Pass parameters and initialize node
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<StaticTransform>(argv));
+  rclcpp::shutdown();
+  return 0;
+}
