5320de67f73194ae7c6a20b998f07d4e5ff982c7
Homalozoa <nx.tardis@gmail.com>
2022-03-17 00:40:13 +0800

    [Modify] Change static_transform.cpp for ch4
    Signed-off-by: Homalozoa <nx.tardis@gmail.com>

--- ch4/tf2/ch4_tf2_cpp/src/static_transform.cpp
+++ ch4/tf2/ch4_tf2_cpp/src/static_transform.cpp
@@ -1,73 +1,84 @@
+// Copyright (c) 2022 Homalozoa
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <memory>
+#include <string>
 
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/static_transform_broadcaster.h"
 
-using std::placeholders::_1;
-
 class StaticTransform : public rclcpp::Node
 {
 public:
-  explicit StaticTransform(char * transformation[])
-  : Node("static_transform_node")
+  explicit StaticTransform(const std::string & node_name, char * argv[])
+  : Node(node_name)
   {
     tf_publisher_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
-    this->make_transforms(transformation);
+    this->set_transform(
+      "world", "robot",  // frame ids
+      1, 2, 3,  // translation
+      90, 0, 0);  // rotation
+    // this->set_transform(
+    //   argv[1], argv[2],  // frame ids
+    //   atof(argv[3]), atof(argv[4]), atof(argv[5]),  // translation
+    //   atof(argv[6]), atof(argv[7]), atof(argv[8]));  // rotation
   }
 
 private:
-  void make_transforms(char * transformation[])
+  void set_transform(
+    const std::string & source_frame,
+    const std::string & target_frame,
+    const double & trans_x,
+    const double & trans_y,
+    const double & trans_z,
+    const double & rot_roll,
+    const double & rot_pitch,
+    const double & rot_yaw)
   {
     rclcpp::Time now = this->get_clock()->now();
-    geometry_msgs::msg::TransformStamped t;
+    geometry_msgs::msg::TransformStamped trans;
+    tf2::Quaternion quat;
 
-    t.header.stamp = now;
-    t.header.frame_id = "world";
-    t.child_frame_id = transformation[1];
+    trans.header.stamp = now;
+    trans.header.frame_id = source_frame;
+    trans.child_frame_id = target_frame;
+    trans.transform.translation.x = trans_x;
+    trans.transform.translation.y = trans_y;
+    trans.transform.translation.z = trans_z;
+    quat.setRPY(rot_roll, rot_pitch, rot_yaw);
+    trans.transform.rotation.x = quat.x();
+    trans.transform.rotation.y = quat.y();
+    trans.transform.rotation.z = quat.z();
+    trans.transform.rotation.w = quat.w();
 
-    t.transform.translation.x = atof(transformation[2]);
-    t.transform.translation.y = atof(transformation[3]);
-    t.transform.translation.z = atof(transformation[4]);
-    tf2::Quaternion q;
-    q.setRPY(
-      atof(transformation[5]),
-      atof(transformation[6]),
-      atof(transformation[7]));
-    t.transform.rotation.x = q.x();
-    t.transform.rotation.y = q.y();
-    t.transform.rotation.z = q.z();
-    t.transform.rotation.w = q.w();
-
-    tf_publisher_->sendTransform(t);
+    tf_publisher_->sendTransform(trans);
   }
   std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_publisher_;
 };
 
 int main(int argc, char * argv[])
 {
-  auto logger = rclcpp::get_logger("logger");
-
-  // Obtain parameters from command line arguments
-  if (argc != 8) {
-    RCLCPP_INFO(
-      logger, "Invalid number of parameters\nusage: "
-      "ros2 run learning_tf2_cpp static_turtle_tf2_broadcaster "
-      "child_frame_name x y z roll pitch yaw");
+  if (argc != 9) {
+    RCLCPP_ERROR(
+      rclcpp::get_logger("Notice"),
+      "Usage: ros2 run ch4_tf2_cpp static_transform header_frame child_frame x y z roll pitch yaw");
     return 1;
   }
-
-  // As the parent frame of the transform is `world`, it is
-  // necessary to check that the frame name passed is different
-  if (strcmp(argv[1], "world") == 0) {
-    RCLCPP_INFO(logger, "Your static turtle name cannot be 'world'");
-    return 1;
-  }
-
-  // Pass parameters and initialize node
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<StaticTransform>(argv));
+  rclcpp::spin(std::make_shared<StaticTransform>("static_tf_node", argv));
   rclcpp::shutdown();
   return 0;
 }
