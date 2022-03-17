e2a5ce10036a94ad9da12802cb8113ea38e75fd5
Homalozoa <nx.tardis@gmail.com>
2022-03-17 18:59:30 +0800

    [New] Complete tf2 for ch4
    Signed-off-by: Homalozoa <nx.tardis@gmail.com>

--- ch4/tf2/ch4_tf2_cpp/src/dynamic_transform.cpp
+++ ch4/tf2/ch4_tf2_cpp/src/dynamic_transform.cpp
@@ -12,83 +12,62 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <chrono>
 #include <memory>
 #include <string>
 
-#include <geometry_msgs/msg/transform_stamped.hpp>
-
-#include <rclcpp/rclcpp.hpp>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_ros/transform_broadcaster.h>
-// #include <turtlesim/msg/pose.hpp>
-
-using std::placeholders::_1;
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/transform_listener.h"
 
 class DynamicTransform : public rclcpp::Node
 {
 public:
-  DynamicTransform()
-  : Node("turtle_tf2_frame_publisher")
+  explicit DynamicTransform(const std::string & node_name)
+  : Node(node_name)
   {
-    // // Declare and acquire `turtlename` parameter
-    // this->declare_parameter<std::string>("turtlename", "turtle");
-    // this->get_parameter("turtlename", turtlename_);
-
-    // // Initialize the transform broadcaster
-    // tf_broadcaster_ =
-    //   std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-
-    // // Subscribe to a turtle{1}{2}/pose topic and call handle_turtle_pose
-    // // callback function on each message
-    // std::ostringstream stream;
-    // stream << "/" << turtlename_.c_str() << "/pose";
-    // std::string topic_name = stream.str();
-
-    // subscription_ = this->create_subscription<turtlesim::msg::Pose>(
-    //   topic_name, 10,
-    //   std::bind(&DynamicTransform::handle_turtle_pose, this, _1));
+    using namespace std::chrono_literals;
+    delta_x = 0.0;
+    tf_publisher_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+    tf_timer_ =
+      this->create_wall_timer(30ms, std::bind(&DynamicTransform::update_transform, this));
   }
 
 private:
-  // void handle_turtle_pose(const turtlesim::msg::Pose & msg)
-  // {
-  //   rclcpp::Time now = this->get_clock()->now();
-  //   geometry_msgs::msg::TransformStamped t;
+  void update_transform()
+  {
+    rclcpp::Time now = this->get_clock()->now();
+    geometry_msgs::msg::TransformStamped trans;
+    tf2::Quaternion quat;
 
-  //   // Read message content and assign it to
-  //   // corresponding tf variables
-  //   t.header.stamp = now;
-  //   t.header.frame_id = "world";
-  //   t.child_frame_id = turtlename_.c_str();
+    trans.header.stamp = now;
+    trans.header.frame_id = "map";
+    trans.child_frame_id = "robot";
+    trans.transform.translation.x = delta_x;
+    trans.transform.translation.y = 0;
+    trans.transform.translation.z = 0;
+    quat.setRPY(0, 0, 0);
+    trans.transform.rotation.x = quat.x();
+    trans.transform.rotation.y = quat.y();
+    trans.transform.rotation.z = quat.z();
+    trans.transform.rotation.w = quat.w();
 
-  //   // Turtle only exists in 2D, thus we get x and y translation
-  //   // coordinates from the message and set the z coordinate to 0
-  //   t.transform.translation.x = msg.x;
-  //   t.transform.translation.y = msg.y;
-  //   t.transform.translation.z = 0.0;
-
-  //   // For the same reason, turtle can only rotate around one axis
-  //   // and this why we set rotation in x and y to 0 and obtain
-  //   // rotation in z axis from the message
-  //   tf2::Quaternion q;
-  //   q.setRPY(0, 0, msg.theta);
-  //   t.transform.rotation.x = q.x();
-  //   t.transform.rotation.y = q.y();
-  //   t.transform.rotation.z = q.z();
-  //   t.transform.rotation.w = q.w();
-
-  //   // Send the transformation
-  //   tf_broadcaster_->sendTransform(t);
-  // }
-  // rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
-  // std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-  // std::string turtlename_;
+    tf_publisher_->sendTransform(trans);
+    delta_x += 0.01;
+  }
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_publisher_;
+  rclcpp::TimerBase::SharedPtr tf_timer_;
+  double delta_x;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DynamicTransform>());
+  rclcpp::spin(std::make_shared<DynamicTransform>("dynamic_tf_node"));
   rclcpp::shutdown();
   return 0;
 }
